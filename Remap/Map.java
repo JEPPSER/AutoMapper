bd4893d8dc23d21b73bd57cfb38068d17e544901
Jesper Bergström <jesper.b96@gmail.com>
2017-01-29 08:53:51 +0100

    Add files via upload

--- Remap/Map.java
+++ Remap/Map.java
@@ -13,7 +13,9 @@ import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.JTextField;
 
 import net.iharder.dnd.FileDrop;
@@ -25,21 +27,36 @@ public class Map extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	// max values for x and y
+	final static int MAX_X = 530;
+	final static int MAX_Y = 310;
+
 	public static void main(String[] args) {
 
 		JButton b1 = new JButton("Generate map");
 		JPanel jp = new JPanel();
 		JFrame jf = new JFrame();
-		JTextField text = new JTextField(30);
+		JTextField map = new JTextField(30);
+		JSlider space = new JSlider(JSlider.HORIZONTAL, 100, 500, 100);
+		JLabel text = new JLabel();
+
+		text.setText("Choose Spacing.   (100 = Complete Auto)");
+		space.setMajorTickSpacing(100);
+		space.setMinorTickSpacing(10);
+		space.setPaintTicks(true);
+		space.setPaintLabels(true);
 
 		jf.setLayout(new FlowLayout());
 
+		jf.add(text);
 		jf.setTitle("AutoMapper");
 		jf.setVisible(true);
 		jf.setSize(400, 200);
-		jp.add(text);
+		jp.add(map);
+		jf.add(space);
 		jf.add(jp);
 		jf.add(b1, BorderLayout.CENTER);
+		jf.setResizable(false);
 		jf.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		// FileDrop class for getting file path.
@@ -47,7 +64,7 @@ public class Map extends JFrame {
 
 			public void filesDropped(java.io.File[] files) {
 
-				text.setText(files[0].getAbsolutePath());
+				map.setText(files[0].getAbsolutePath());
 			}
 		});
 
@@ -56,27 +73,32 @@ public class Map extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent evt) {
 
-				String path = text.getText();
+				int spacing = space.getValue();
+
+				String path = map.getText();
+
+				Random rand = new Random();
 
 				path = path.replace("\"", "");
 				path = path.replace("\\", "\\\\");
-
-				File file = new File(path);
 
 				Scanner scan;
 
 				try {
 
+					File file = new File(path);
+
 					String[] pathPart = path.split("\\[");
 					String newPath = pathPart[0] + "[auto map].osu";
 
+					// making a new file
 					PrintWriter writer = new PrintWriter(newPath, "UTF-8");
 
 					scan = new Scanner(file, "utf-8");
-					Random rand = new Random();
 
 					double time = 0;
 					int tmpX = 0, tmpY = 0;
+					String[] prevParts = new String[6];
 					String temp;
 					boolean found = false;
 
@@ -85,6 +107,7 @@ public class Map extends JFrame {
 
 						temp = scan.nextLine();
 
+						// changing name of version
 						if (temp.length() >= 8 && temp.substring(0, 7).equals("Version")) {
 							String[] parts = temp.split(":");
 							temp = parts[0] + ":auto map";
@@ -97,19 +120,44 @@ public class Map extends JFrame {
 						}
 					}
 
+					boolean wasStream = true;
 					// Changing the x and y values.
 					while (scan.hasNextLine()) {
 						temp = scan.nextLine();
+
 						String result = "";
 						String[] parts = temp.split(",");
 
+						// if not stream and not slider
 						if (Double.parseDouble(parts[2]) - time > 100 && parts.length <= 6) {
-							parts[0] = String.valueOf(rand.nextInt(450 + 50));
-							parts[1] = String.valueOf(rand.nextInt(250 + 65));
-						} else if (Double.parseDouble(parts[2]) - time <= 100 && parts.length > 6
-								|| parts.length <= 6) {
+							if (spacing == 100) {
+								String[] tmpStr = totallyRandom().split(",");
+								parts[0] = tmpStr[0];
+								parts[1] = tmpStr[1];
+							}
+							// if last object was stream or slider
+							else if (prevParts.length > 6 || wasStream) {
+
+								parts[0] = "1";
+								parts[1] = String.valueOf(rand.nextInt(250) + 25);
+
+							} else {
+								String[] tmpStr = chooseSpacing(spacing, tmpX, tmpY).split(",");
+								parts[0] = tmpStr[0];
+								parts[1] = tmpStr[1];
+							}
+							wasStream = false;
+						}
+						// if stream and slider or if just stream
+						else if (Double.parseDouble(parts[2]) - time <= 100 && parts.length > 6 || parts.length <= 6) {
 							parts[0] = String.valueOf(tmpX);
 							parts[1] = String.valueOf(tmpY);
+
+							// if it was a stream
+							if (parts.length > 6)
+								wasStream = false;
+							else
+								wasStream = true;
 						}
 
 						for (int i = 0; i < parts.length - 1; i++) {
@@ -119,9 +167,11 @@ public class Map extends JFrame {
 
 						// print new x and y values
 						writer.println(result);
+
 						time = Double.parseDouble(parts[2]);
 						tmpX = Integer.parseInt(parts[0]);
 						tmpY = Integer.parseInt(parts[1]);
+						prevParts = parts;
 
 					}
 
@@ -138,5 +188,54 @@ public class Map extends JFrame {
 				}
 			}
 		});
+	}
+
+	// returns string of a random value.
+	public static String totallyRandom() {
+
+		Random rand = new Random();
+
+		return String.valueOf(rand.nextInt(MAX_X)) + "," + String.valueOf(rand.nextInt(MAX_Y));
+	}
+
+	// returns string of x and y values with custom set spacing.
+	public static String chooseSpacing(int spacing, int oldX, int oldY) {
+
+		Random rand = new Random();
+
+		// set new x value.
+		int nwX = rand.nextInt(2 * spacing) + oldX - spacing;
+
+		// make sure its in range.
+		while (nwX < 0 || nwX > MAX_X) {
+			if (nwX < 0 || nwX > MAX_X)
+				nwX = rand.nextInt(2 * spacing) + oldX - spacing;
+		}
+
+		// Start calculating where the new y value should be, relative to the
+		// spacing and x value.
+		int xLength = nwX - oldX;
+
+		double yLength = xLength * xLength - spacing * spacing;
+
+		int randomY = rand.nextInt(2);
+		if (randomY == 0)
+			randomY = -1;
+
+		yLength = Math.sqrt(-1 * yLength) * randomY;
+
+		int nwY = (int) yLength + oldY; // done calculating!
+
+		// if the new circle is not in the playfield, return new values.
+		if (nwX > 0 && nwX < MAX_X && nwY > 0 && nwY < MAX_Y) {
+
+			return String.valueOf(nwX) + "," + String.valueOf(nwY);
+
+		}
+		// else, run method again.
+		else {
+
+			return chooseSpacing(spacing, oldX, oldY);
+		}
 	}
 }
