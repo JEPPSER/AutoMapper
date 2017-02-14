d4c82a656739673e3578f3a21a0b1ffd413ec041
Jesper Bergström <jesper.b96@gmail.com>
2017-02-14 09:43:40 +0100

    Added 2 new modes

--- Remap/Map.java
+++ Remap/Map.java
@@ -11,10 +11,12 @@ import java.io.UnsupportedEncodingException;
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
 
@@ -30,6 +32,9 @@ public class Map extends JFrame {
 	// max values for x and y
 	final static int MAX_X = 530;
 	final static int MAX_Y = 310;
+	
+	static int mode = 2;
+	static int count = 0;
 
 	public static void main(String[] args) {
 
@@ -39,25 +44,84 @@ public class Map extends JFrame {
 		JTextField map = new JTextField(30);
 		JSlider space = new JSlider(JSlider.HORIZONTAL, 100, 500, 100);
 		JLabel text = new JLabel();
+		JRadioButton isCustom = new JRadioButton("Set Custom Spacing                                                ");
+		JRadioButton isRandom = new JRadioButton("Completly Random                                                  ");
+		JRadioButton isInherit = new JRadioButton("Inherit Spacing                                                         ");
+		JRadioButton isSquare = new JRadioButton("Only Square Jumps                                                ");
+		isRandom.setSelected(true);
 
-		text.setText("Choose Spacing.   (100 = Complete Auto)");
+		text.setText("Choose Spacing:");
+		text.setVisible(false);
 		space.setMajorTickSpacing(100);
 		space.setMinorTickSpacing(10);
 		space.setPaintTicks(true);
 		space.setPaintLabels(true);
+		space.setVisible(false);
 
 		jf.setLayout(new FlowLayout());
 
 		jf.add(text);
 		jf.setTitle("AutoMapper");
 		jf.setVisible(true);
-		jf.setSize(400, 200);
+		jf.setSize(400, 350);
 		jp.add(map);
 		jf.add(space);
+		jf.add(isCustom);
+		jf.add(isRandom);
+		jf.add(isInherit);
+		jf.add(isSquare);
 		jf.add(jp);
 		jf.add(b1, BorderLayout.CENTER);
 		jf.setResizable(false);
 		jf.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+        ButtonGroup group = new ButtonGroup();
+        group.add(isCustom);
+        group.add(isRandom);
+        group.add(isInherit);
+        group.add(isSquare);
+       
+        //action listeners for radiobuttons
+        isCustom.addActionListener(new ActionListener() {
+ 
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+               
+                mode = 1;
+                text.setVisible(true);
+                space.setVisible(true);
+            }
+        });
+        isRandom.addActionListener(new ActionListener() {
+ 
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+               
+                mode = 2;
+                text.setVisible(false);
+                space.setVisible(false);
+            }
+        });
+        isInherit.addActionListener(new ActionListener() {
+ 
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+               
+                mode = 3;
+                text.setVisible(false);
+                space.setVisible(false);
+            }
+        });
+        isSquare.addActionListener(new ActionListener() {
+ 
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+               
+                mode = 4;
+                text.setVisible(false);
+                space.setVisible(false);
+            }
+        });
 
 		// FileDrop class for getting file path.
 		new FileDrop(jp, new FileDrop.Listener() {
@@ -119,29 +183,48 @@ public class Map extends JFrame {
 							found = true;
 						}
 					}
-
+					
 					boolean wasStream = true;
 					// Changing the x and y values.
 					while (scan.hasNextLine()) {
 						temp = scan.nextLine();
-
+						
 						String result = "";
 						String[] parts = temp.split(",");
 
 						// if not stream and not slider
 						if (Double.parseDouble(parts[2]) - time > 100 && parts.length <= 6) {
-							if (spacing == 100) {
+							if (mode == 2) {
 								String[] tmpStr = totallyRandom().split(",");
 								parts[0] = tmpStr[0];
 								parts[1] = tmpStr[1];
 							}
+							else if(mode == 3){
+								int tmpSpace = sameSpacing(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), tmpX, tmpY);
+								String[] tmpStr = chooseSpacing(tmpSpace, tmpX, tmpY).split(",");
+								parts[0] = tmpStr[0];
+								parts[1] = tmpStr[1];
+							}
+							else if(mode == 4){
+								if(count > 0){
+									String[] tmpStr = squareJumps(tmpX, tmpY).split(",");
+									parts[0] = tmpStr[0];
+									parts[1] = tmpStr[1];
+								}
+							}
+							
 							// if last object was stream or slider
-							else if (prevParts.length > 6 || wasStream) {
+							else if (prevParts.length > 6 && spacing > 300 || wasStream && spacing > 300) {
 
-								parts[0] = "1";
-								parts[1] = String.valueOf(rand.nextInt(250) + 25);
+								int r = rand.nextInt(2);
+								if (r == 0)
+									r = -1;
+								
+								parts[0] = String.valueOf(MAX_X/2 + spacing*r/2);
+								parts[1] = String.valueOf(rand.nextInt(MAX_Y));
 
-							} else {
+							} else if(mode == 1){
+		
 								String[] tmpStr = chooseSpacing(spacing, tmpX, tmpY).split(",");
 								parts[0] = tmpStr[0];
 								parts[1] = tmpStr[1];
@@ -172,7 +255,9 @@ public class Map extends JFrame {
 						tmpX = Integer.parseInt(parts[0]);
 						tmpY = Integer.parseInt(parts[1]);
 						prevParts = parts;
-
+						
+						count++;
+						
 					}
 
 					writer.close();
@@ -237,5 +322,48 @@ public class Map extends JFrame {
 
 			return chooseSpacing(spacing, oldX, oldY);
 		}
+	}
+	public static int sameSpacing(int x, int y, int oldX, int oldY) {
+		
+		int res;
+		
+		res = (int) Math.sqrt((oldY-y)*(oldY-y) + (oldX-x)*(oldX-x));
+		
+		return res;
+	}
+
+	public static String squareJumps(int oldX, int oldY){
+		
+		String res;
+		int x;
+		int y;
+		
+		if(count % 4 == 1){
+			x = oldX - 20;
+			y = oldY - 220;
+		}
+		else if(count % 4 == 2){
+			x = oldX - 200;
+			y = oldY;
+		}
+		else if(count % 4 == 3){
+			x = oldX;
+			y = oldY + 200;
+		}
+		else{
+			x = oldX + 200;
+			y = oldY;
+		}
+		
+		if(x>MAX_X || x<0 || y>MAX_Y || y<0){
+			
+			count = 3;
+			x = 200;
+			y = 300;
+		}
+	
+		res = x + "," + y;
+			
+		return res;		
 	}
 }
